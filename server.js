514e8160ba7eceb529d293d44850ed4e5140add1
Riaz N Virani <riaz.n.virani@gmail.com>
2017-06-28 14:52:23 -0400

    Fix previous commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV.toLowerCase() !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
