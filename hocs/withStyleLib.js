5a0793b839eefe6ab2273111077f1a9077870bf3
Riaz N Virani <riaz.n.virani@gmail.com>
2017-07-07 13:35:40 -0400

    Add title

--- hocs/withStyleLib.js
+++ hocs/withStyleLib.js
@@ -7,6 +7,7 @@ export default BaseComponent => props => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Hexient Labs</title>
       {/* START Favicons */}
       <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
       <link rel="apple-touch-icon" sizes="57x57" href="/static/apple-icon-57x57.png" />
